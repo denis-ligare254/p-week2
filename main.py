7bb7ed0a9f6193ebf037cd6ef8799c132abc85ca
denis <mrisholigare@gmail.com>
2021-02-22 21:34:45 +0300

    more workings on the index page

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template
-
+from newsapi import NewsApiClient 
 
 app = Flask(__name__)
 @app.route('/')
 def index ():
-   return "<h1>hellow world</h1>"
+   newsapi =NewsApiClient(api_key="0e1fc7cee0e8462e92dd4bb8eb5f98c1")
    NewsKey = NewsApiKey(api_key="0e1fc7cee0e8462e92dd4bb8eb5f98c1")
    '''
    unique key to help us access news over the internet to the application
    '''
-  
+  bulletingMain =  newsapi.get_bulleting_Main(sources="abc-news-au")
 
 
 
