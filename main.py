73958283f151bde50572b37a04a878d86ab050c7
denis <mrisholigare@gmail.com>
2021-02-22 18:24:10 +0300

    news api key created

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+@app.route('/')
+def index ():
+   NewsKey = NewsApiKey(api_key="0e1fc7cee0e8462e92dd4bb8eb5f98c1")
+   '''
+   unique key to help us access news over the internet
+   '''
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
