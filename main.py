ee4353ce2606a21a44a56c9e5a465c58bb311fef
denis <mrisholigare@gmail.com>
2021-02-23 14:29:00 +0300

    static file put inside the project

--- main.py
+++ main.py
@@ -1,35 +1,4 @@
-# from flask import Flask, render_template
-# from newsapi import NewsApiClient 
 
-# app = Flask(__name__)
-# @app.route('/')
-# def index ():
-#    newsapi =NewsApiClient(api_key="0e1fc7cee0e8462e92dd4bb8eb5f98c1")
-#    NewsKey = NewsApiKey(api_key="0e1fc7cee0e8462e92dd4bb8eb5f98c1")
-#    '''
-#    unique key to help us access news over the internet to the application
-#    '''
-#    bulletingMain =  newsapi.get_bulleting_Main(sources="abc-news-au")
-
-#    articles = bulletingMain("articles")
- 
-#    description =[]
-#    news =[]
-#    images =[]
-
-# for i in range(len(articles)):
-#     myarticles
-#     news.append(myarticles['title'])
-#     description.append(myarticles['description'])
-#     images.append(myarticles['urlToImage'])
-
-
-# mylist =zip(description ,news, images)
-
-# return render_template('index.html' context = mylist)
-
-# if __name__ == '__main__':
-#     app.run(debug = True)
 from flask import Flask, render_template
 from newsapi import NewsApiClient
 
@@ -40,7 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def Index():
     newsapi = NewsApiClient(api_key="b0f75ce660c0466a9a98c2478f8abb62")
-    topheadlines = newsapi.get_top_headlines(sources="al-jazeera-english")
+    topheadlines = newsapi.get_top_headlines(sources="abc-news-au")
 
     articles = topheadlines['articles']
 
@@ -63,22 +32,22 @@ def Index():
 @app.route('/bbc')
 def bbc():
     newsapi = NewsApiClient(api_key="YOUR-API-KEY")
-    topheadlines = newsapi.get_top_headlines(sources="bbc-news")
+    topheadlines = newsapi.get_top_headlines(sources="ary-news")
 
     articles = topheadlines['articles']
 
-    desc = []
+    description = []
     news = []
-    img = []
+    image= []
 
     for i in range(len(articles)):
         myarticles = articles[i]
 
         news.append(myarticles['title'])
-        desc.append(myarticles['description'])
-        img.append(myarticles['urlToImage'])
+        description.append(myarticles['description'])
+        image.append(myarticles['urlToImage'])
 
-    mylist = zip(news, desc, img)
+    mylist = zip(news, description, image)
 
     return render_template('bbc.html', context=mylist)
 
